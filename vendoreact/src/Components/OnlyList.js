10fd79105ce838c3889365106e0cd3856728e283
Debashish-Dey-DM <deydebashish71@gmail.com>
2021-10-31 10:47:42 +0600

    specified user list

--- vendoreact/src/Components/OnlyList.js
+++ vendoreact/src/Components/OnlyList.js
@@ -8,22 +8,38 @@ import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import DoneIcon from '@mui/icons-material/Done';
+import ClearIcon from '@mui/icons-material/Clear';
+import Stack from '@mui/material/Stack';
 export default function OnlyList() {
+  const [current, setcurrent] = useState("");
   const [user, setuser] = useState([]);
-       const mount = async () => {
-        const res = await axios.get('http://localhost:8000/api/user/UserList');
+  const mount = async () => {
+         const data = JSON.parse(localStorage.getItem("user-info"));
+         if (data) {
+           console.log(data.name);
+           setcurrent(data.name);
+           const id = data.id;
+
+        const res = await axios.get(`http://localhost:8000/api/user/UserList`);
         console.log(res.data);
-        
+
         if (res.status === 200) {
             setuser(res.data)
             
         }
+
+           
+        }
+
   }
   
     useEffect(() => {
     mount();
         
-     }, []);
+    }, []);
+  
+  
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       {
@@ -32,10 +48,10 @@ export default function OnlyList() {
             <>
               <ListItem alignItems="flex-start">
         <ListItemAvatar>
-          <Avatar alt={e.name}src="/static/images/avatar/1.jpg" />
+                  <Avatar alt={!(current === e.name)?e.email: <> <del>{e.name}</del> </> } src="/static/images/avatar/1.jpg" />
         </ListItemAvatar>
         <ListItemText
-          primary={e.name}
+          primary={!(current === e.name)?e.name: <> <del>{e.name}</del> </> }
           secondary={
             <React.Fragment>
               <Typography
@@ -44,13 +60,29 @@ export default function OnlyList() {
                 variant="body2"
                 color="text.primary"
               >
-                
+               
               </Typography>
-              {e.email}
+              {!(current === e.name) ? e.email : <> <del>{e.email}</del> </>}
+
+              {
+                !(current === e.name) ?
+                  
+              <Stack direction="row" spacing={4}>
+                <ClearIcon />
+                 <DoneIcon/>
+                  </Stack>
+                  :
+                  <h5>Damm its you man</h5>
+                }
+              <div>
+                
+                
+              </div>
             </React.Fragment>
           }
         />
-      </ListItem>
+              </ListItem>
+              
       <Divider variant="inset" component="li" />
             </>
           );
